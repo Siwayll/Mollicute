286d9147e053775c213699c591834a7b62313474
Adrien <aimbert@solire.fr>
2013-12-20 14:32:20 +0100

    réstauration de config.php

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Base d'une aspiration
+ *
+ * @author  Siwaÿll <sanath.labs@gmail.com>
+ * @license CC by-nc http://creativecommons.org/licenses/by-nc/3.0/fr/
+ */
+namespace Siwayll\Mollicute;
+
+/**
+ * Base d'une aspiration
+ *
+ * @author  Siwaÿll <sanath.labs@gmail.com>
+ * @license CC by-nc http://creativecommons.org/licenses/by-nc/3.0/fr/
+ */
+class Config
+{
+    /**
+     * Création d'une configuration d'aspiration
+     */
+    public function __construct()
+    {
+    }
+}
